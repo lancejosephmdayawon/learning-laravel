303e8483a96e57d553939cab59c5dafccaa0d529
lancejosephmdayawon <lancejosephmdayawon@gmail.com>
2025-10-16 16:06:49 +0800

    Added Bootstrap and hello.blade.php

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Page</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    <button type="button" class="btn btn-primary">Base class</button>
+
+</body>
+
+</html>
